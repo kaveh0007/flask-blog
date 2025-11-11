a49ca620c342c2e8a6b06aea124ae5181be0b185
Kishlaya Verma <vermakishlaya@protonmail.com>
2025-11-12 00:11:08 +0530

    Create login page and logic after user signup/login

--- flask_blog.py
+++ flask_blog.py
@@ -1,43 +1,59 @@
-from flask import Flask, render_template
-from forms import RegistrationForm
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'cbd535a731c27deae6fc9e9c2fbb12225cb321c530fddcf272c6a606b24dca58'
+app.config["SECRET_KEY"] = (
+    "cbd535a731c27deae6fc9e9c2fbb12225cb321c530fddcf272c6a606b24dca58"
+)
 
 Posts = [
     {
-        'author': 'Kaveh',
-        'title': 'Blog Post 1',
-        'date_posted': 'October 24, 2025',
-        'content': 'Hello this is my first blog post'
+        "author": "Kaveh",
+        "title": "Blog Post 1",
+        "date_posted": "October 24, 2025",
+        "content": "Hello this is my first blog post",
     },
-
     {
-        'author': 'John Doe',
-        'title' : 'Blog Post 1',
-        'date_posted' : 'October 24, 2025',
-        'content' : 'Hello now the AWS server in east US just went down'
-    }
+        "author": "John Doe",
+        "title": "Blog Post 1",
+        "date_posted": "October 24, 2025",
+        "content": "Hello now the AWS server in east US just went down",
+    },
 ]
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html', posts=Posts)
+    return render_template("home.html", posts=Posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
 
-@app.route('/health')
+
+@app.route("/health")
 def health_check():
-    return 'The backend is running'
+    return "The backend is running"
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', form=form, title='Sign Up')
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", form=form, title="Sign Up")
 
-@app.route('/login')
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html', title='Login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "kaveh@blog.com" and form.password.data == "password":
+            flash("Logged in Successfully!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid Credentials!", "danger")
+    return render_template("login.html", form=form, title="Login")
--- forms.py
+++ forms.py
@@ -10,12 +10,17 @@ class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[InputRequired(), Email()])
     password = PasswordField("Password", validators=[InputRequired()])
     confirm_password = PasswordField(
-        "Confirm Password", validators=[InputRequired(), EqualTo('password', message='Passwords do not match!')])
+        "Confirm Password",
+        validators=[
+            InputRequired(),
+            EqualTo("password", message="Passwords do not match!"),
+        ],
+    )
     submit = SubmitField("Sign up")
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email()])
     password = PasswordField("Password")
-    rememberMe = BooleanField()
+    remember = BooleanField()
     submit = SubmitField("Login")
